6bb697e45c2e75c0cc9c8db8d1b703cb7fd15154
Vyacheslav Voronenko <git@voronenko.info>
2018-11-28 15:35:47 +0100

    docker, py

---
+++ docker/provision_box.sh
@@ -0,0 +1,23 @@
+# Static parameters
+WORKSPACE=./
+BOX_PLAYBOOK=$WORKSPACE/box.yml
+BOX_NAME=docker_role
+BOX_ADDRESS=$EXTERNALBOX
+BOX_USER=$EXTERNALUSER
+BOX_PWD=
+
+prudentia ssh <<EOF
+unregister $BOX_NAME
+
+register
+$BOX_PLAYBOOK
+$BOX_NAME
+$BOX_ADDRESS
+$BOX_USER
+$BOX_PWD
+
+verbose 4
+set box_address $BOX_ADDRESS
+
+provision $BOX_NAME
+EOF
